e68a70ccc624999bcd9db6ba92db304e5b3d9c9e
RavinduOnline <ravindurasanga99@gmail.com>
2022-05-26 13:56:23 +0530

    Reply Serch implemented

--- client/src/components/Pages/Reply/profile reply/profilereply.js
+++ client/src/components/Pages/Reply/profile reply/profilereply.js
@@ -3,6 +3,8 @@ import '../replycard/replycard.css'
 
 export default function Profilereply() {
     const [ReplyObj , setReplyObj] = useState([])
+    const [SearchWord , setSearchWord] = useState("");
+
 
     useEffect(() => {
       getreply();
@@ -24,10 +26,55 @@ export default function Profilereply() {
         })
     }
 
+       /*--------------------------------- Search FU Start -------------------------------*/
+
+       useEffect(() => {
+        if(SearchWord){
+          handleSearchArea();
+        }
+        if(SearchWord === ""){
+            getreply();
+        }
+  }, [SearchWord]);
+          
+ const   handleSearchArea = () =>{
+
+    const userDetails = JSON.parse(localStorage.getItem("user"))
+    console.log(userDetails._id)
+                  
+        fetch("/reply/user/"+userDetails._id).then(res=>res.json())
+              .then(result =>{
+                if(result){
+                  filterData(result,SearchWord.toLowerCase());
+                }
+
+              });
+  }
+
+  const filterData = (ReplyObj,searchKey) => {
+
+    const result = ReplyObj.filter((ReplyObj) =>
+          ReplyObj.reply.toLowerCase().includes(searchKey)      
+        );
+        setReplyObj(result)
+
+  }
+
+
+/*--------------------------------- Search FU End -------------------------------*/
+
+
   return (
     <div>
-         <input id="profile-searchNav" type="search" placeholder="Search" aria-label="Search" /> 
-         
+         <input 
+            id="profile-searchNav" 
+            type="search" 
+            placeholder="Search" 
+            aria-label="Search" 
+            value={SearchWord}
+            onChange={(e) => setSearchWord(e.target.value)}
+            /> 
+
         { ReplyObj.map(getReplies=> (
 
         <div className='replycard-mainbox'>
